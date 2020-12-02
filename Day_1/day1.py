bd178ecff1ba87557ddced8556c008cd0202dbff
Aodhan Dalton <aodhan.dalton@rewardinsight.com>
2020-12-02 00:33:31 +0000

    performance improvments

--- Day_1/day1.py
+++ Day_1/day1.py
@@ -4,16 +4,18 @@ import time
 def a(x,file):
     for i in file:
         j = x - i
-        if j in file:
-            return i * j
+        if j > 0:
+            if j in file:
+                return i * j
 
 # Part B
 def b(x,file):
     for i in file:
         for k in file:
-            j = x - i - k
-            if j in file:
-                return i * j * k
+            j = x - i - k 
+            if j > 0:
+                if j in file:
+                    return i * j * k
 
 with open('day1') as file:
   nums = [int(i.strip()) for i in file]
